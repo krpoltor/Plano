e0ac1134a03a72158fcebb2f9186cfa06d4474d9
Krzysztof P <krzysztof.poltorak@protonmail.com>
2018-02-07 17:20:07 +0100

    first commit

---
+++ plano/src/main/java/com/dev/plano/Startup.java
@@ -0,0 +1,13 @@
+package com.dev.plano;
+
+/**
+ * Hello world!
+ *
+ */
+public class Startup 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
